bc8bb64dbda25d7d704d70caaa9ea192b7ee3927
Andrii Skvortsov <andrii.skvortsov@ericsson.com>
2018-11-17 17:18:32 +0200

    added fcm configuration file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/5.5.0/firebase-messaging.js');
  firebase.initializeApp({
    'messagingSenderId': '624325475971'
  });
- debugger;
 const messaging = firebase.messaging();
 
 // If you would like to customize notifications that are received in the
